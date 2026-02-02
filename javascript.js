e3e987e333c18e60112b2cb5ab2bfa9de0769bff
yogigit2k5 <sakshiyogi22@gmail.com>
2026-02-02 12:13:19 +0530

    added promise and settimeout code

--- javascript.js
+++ javascript.js
@@ -59,12 +59,125 @@
 // },1000);
 //  ABOVE CODE IS CORRECT ( AS PER MY COMPUTER)
 // (OR SIR WALA )
-let count=0;
+// let count=0;
 
-const interval= setInterval(()=>{
-    count+=1
-    console.log(count);
-    if(count===10){
-        clearInterval(interval);
-    }
-},1000);
+// const interval= setInterval(()=>{
+//     count+=1
+//     console.log(count);
+//     if(count===10){
+//         clearInterval(interval);
+//     }
+// },1000);
+
+
+
+// const studentname = document.querySelector("#myInput");
+// const button = document.querySelector("#myButton");
+// const list = document.querySelector(".list");
+
+// button.addEventListener('click',()=>{   
+//     const li=document.createElement('li');
+//     li.innerText=studentname.value;   
+//     list.appendChild(li);
+//     studentname.value="";
+// }
+// )
+
+// create the elements
+// const li = document.createElement('li');
+// const deleteBtn = document.createElement('button');
+
+// // Fixed: Changed studentsName to studentname to match your earlier variable
+// li.innerText = studentname.value; 
+// deleteBtn.innerText = "Delete";
+
+// deleteBtn.addEventListener('click', () => {
+//     list.removeChild(li);
+// });
+
+// li.appendChild(deleteBtn);
+// list.appendChild(li);
+
+// // Fixed: Changed studentsName to studentname here as well
+// studentname.value = "";
+// (30th  jan)
+// function greet(print){
+
+//     setTimeout(()=>{
+//         console.log("Hello, World!");
+//         let name="John";
+//         print(name);
+//     },2000);
+// }
+
+// greet(print);
+
+// callback function
+console.log("starting homework");
+
+setTimeout(() => {
+    console.log("Homework completed");
+        console.log("Dinner is ready"); 
+
+    setTimeout(() => {
+        console.log("Dinner done");
+        console.log("getting to go out");
+    
+        setTimeout(() => {
+            console.log("going out");
+        }, 1000);
+
+    
+}, 1500);
+}, 2000);
+
+// callback hell
+// function finishHomework(callback){
+//     console.log("starting homework");
+//     setTimeout(() => {
+//         console.log("Homework completed");
+//         callback();
+//     }   , 2000);    
+
+// }
+
+// function eatDinner(callback){
+//     console.log("Dinner is ready"); 
+//     setTimeout(() => {
+//         console.log("Dinner done");
+//         callback();
+//     }   , 1500);
+// }
+
+// function goOut(){
+//     console.log("getting to go out");
+// }
+
+// finishHomework(()=>{
+//     eatDinner(()=>{     
+//         goOut();
+//     });
+// });
+
+const p = new Promise((res,rej)=>{
+    console.log("going to do homework");
+
+    setTimeout(() => {
+        const done=true;
+        if(done){
+            res("Success");
+        }else{
+            rej("Failure to fetch Data from server");
+        
+        } 
+    }, 2000);
+});
+
+p.then((a)=>{
+    console.log(a);
+    
+}) 
+.catch((err)=>{
+    console.log(err);
+});
+
