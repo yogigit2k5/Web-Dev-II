187b8a3cf1a0c9a4508175dd42940236b3d1d3b8
yogigit2k5 <sakshiyogi22@gmail.com>
2026-02-25 20:25:41 +0530

    api

--- javascript.js
+++ javascript.js
@@ -346,18 +346,56 @@
 
 // (  ASSIGNMENT 2 HINT)
 const API_KEY=7e75caf90525cfec824355a26aeabe3b;
-const city= document.querySelector("#city");
-const search= document.querySelector("#search");
+
+const search= document.querySelector("#weatherform");
+const conatiner=document.querySelector(".info");
+
+
+lett visitedCities=[]
+
+search.addEventListener('submit',async(e)=>{
+    e.preventDefault();
+    const data=city.value
+    if(data){
+    try{
+    const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${API_KEY}`);
+    const weatherData=await response.json();
+        
+
+    if(weatherData.cod===200){
+    
+        conatiner.innerHTML=  `<p>City: ${weatherData.name}</p>
+<p>Temp: ${(weatherData.main.temp-273.15).toFixed(1)} C</p>
+<p>Weather: ${weatherData.weather[0].main}</p>
+<p>Humidity: ${weatherData.main.humidity}</p>
+<p>Wind Speed: ${weatherData.wind.speed} miles/hr</p>`;
+
+        visitedCities.push(data)
+        localStorage.setItem("visitedCities",JSON.stringify(visitedCities));
+
+}else{
+    container.innerHTML=`<h3>Weather Info  </h3>
+    <p>${weatherData.message}</p>
+    }
+}catch(e){
+    console.log(e);
+}       
+    }
+
+
+
+
 
 search.addEventListener('click',async(e)=>{
     e.preventDefault();
     const data=city.value
-    const response= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${API_KEY}`);
-    const res= await response.json();
-    console.log(weatherData);
-    console.log("Temp",(weatherData.main.temp-273.15).toFixed(1),"C");
-    console.log("weather:",weatherData.weather[0].main);
-    console.log("humidity:",weatherData.main.humidity);
-    console.log("wind:",weatherData.wind.speed);
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${data}&appid=${API_KEY}`);
+        const res = await response.json();
+        console.log(res);
+    }catch(err){
+        console.log(err);
+    }
 })
-// done
+
+// fetch(`https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=${API_KEY}`)
